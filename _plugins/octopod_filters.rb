03ba209288e26db747f32e1e8bbe9edb6034eeab
Arne Eilermann <eilermann@lavabit.com>
2012-06-04 00:01:11 +0200

    A little fix, some adjustments and a little more metadata to the audio file.

--- _plugins/octopod_filters.rb
+++ _plugins/octopod_filters.rb
@@ -1,3 +1,5 @@
+require 'erb'
+
 module Jekyll
   module OctopodFilters
 
@@ -29,9 +31,45 @@ module Jekyll
       first.nil? || first.empty? ? second : first
     end
 
+    # Returns an <audio> tag for the given file. As a second argument it takes
+    # one of the three possible preload behaviors auto/metadata/none.
+    #
+    # {{ 'audiofile.m4a' | audio }}
     def audio(filename, preload = nil)
-      preload ||= 'auto'
+      return if filename.nil?
+      preload ||= 'none'
       %Q{<audio src="/files/episodes/#{ERB::Util.url_encode(filename)}" preload="#{preload}" />}
+    end
+
+    # Gets a number of seconds and returns an human readable duration string of
+    # it.
+    #
+    # {{ 1252251 | string_of_duration }} => "00:03:13"
+    def string_of_duration(duration)
+      seconds = duration.to_i
+      minutes = seconds / 60
+      hours   = minutes / 60
+
+      "#{"%02d" % hours}:#{"%02d" % (minutes % 60)}:#{"%02d" % (seconds % 60)}"
+    end
+
+    # Gets a number of bytes and returns an human readable string of it.
+    #
+    # {{ 1252251 | string_of_size }} => "1.19M"
+    def string_of_size(bytes)
+      bytes = bytes.to_i.to_f
+      out = '0'
+      return out if bytes == 0.0
+
+      jedec = %w[b K M G]
+      [3, 2, 1, 0].each { |i|
+        if bytes > 1024 ** i
+          out = "%.2f#{jedec[i]}" % (bytes / 1024 ** i)
+          break
+        end
+      }
+
+      return out
     end
 
   end
