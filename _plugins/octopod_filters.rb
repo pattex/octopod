d5c76d55640b26a3bda5f163e0bb711a0520948f
Arne Eilermann <eilermann@lavabit.com>
2012-07-11 12:56:00 +0200

    (Hopefully) Fixed the broken DISQUS integration.

--- _plugins/octopod_filters.rb
+++ _plugins/octopod_filters.rb
@@ -115,17 +115,24 @@ module Jekyll
     end
 
     # Generates the config for disqus integration
-    def disqus_config(site, page)
-      disqus_vars = {
-        'disqus_developer'   => site['disqus_developer'],
-        'disqus_shortname'   => site['disqus_shortname'],
-        'disqus_identifier'  => page['url'],
-        'disqus_url'         => site['url'] + page['url'],
-        'disqus_category_id' => page['disqus_category_id'] || site['disqus_category_id'],
-        'disqus_title'       => page['title'] || site['site']
-      }
-      disqus_vars.delete_if { |_, v| v.nil? }
+    # If a page object is given, it generates the config variables only for this
+    # page. Otherwise it generate only the global config variables.
+    def disqus_config(site, page = nil)
+      if page
+        disqus_vars = {
+          'disqus_identifier'  => page['url'],
+          'disqus_url'         => site['url'] + page['url'],
+          'disqus_category_id' => page['disqus_category_id'] || site['disqus_category_id'],
+          'disqus_title'       => page['title'] || site['site']
+        }
+      else
+        disqus_vars = {
+          'disqus_developer'   => site['disqus_developer'],
+          'disqus_shortname'   => site['disqus_shortname']
+        }
+      end
 
+      disqus_vars.delete_if { |_, v| v.nil? }
       disqus_vars.map { |k, v| "var #{k} = '#{v}';" }.compact.join("\n")
     end
 
