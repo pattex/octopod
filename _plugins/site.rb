cb2b11a35c5a3b1a2da5c5b95330f8d3b2b6866a
Arne Eilermann <eilermann@lavabit.com>
2013-03-01 21:31:28 +0100

    Consider that looking for three letter filename extensions is not super smart in the case of opus. =^.^=

--- _plugins/site.rb
+++ _plugins/site.rb
@@ -4,9 +4,9 @@ module Jekyll
     alias_method :_octopod_original_process, :process
 
     def process
-      feeds = Dir["episodes.???.rss"]
+      feeds = Dir["episodes.{???,????}.rss"]
       config.update(
-        'episode_feed_formats' => feeds.map { |f| f.match(/episodes\.(\w{3})\.rss/)[1] }
+        'episode_feed_formats' => feeds.map { |f| f.match(/episodes\.(\w{3,4})\.rss/)[1] }
       )
 
       _octopod_original_process
